c4bd64cf96015d86a5245364e3c25cd3f39ad44e
InJae Lee <69920975+oliver0922@users.noreply.github.com>
2021-06-14 16:17:18 +0900

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -22,16 +22,16 @@ LossTuple = namedtuple('LossTuple',
                         ])
 # namedtuple을 이용해 dictionary와 같은 형태로 key 값들을 선언한다.
 
+# nn.Module 상속
 class FasterRCNNTrainer(nn.Module):
     """wrapper for conveniently training. return losses
 
     The losses include:
 
-    * :obj:`rpn_loc_loss`: The localization loss for \
-        Region Proposal Network (RPN).
-    * :obj:`rpn_cls_loss`: The classification loss for RPN.
-    * :obj:`roi_loc_loss`: The localization loss for the head module.
-    * :obj:`roi_cls_loss`: The classification loss for the head module.
+    * :obj:`rpn_loc_loss`: RPN bounding box의 regression loss(bounding box의 위치에 관한 손실함수)
+    * :obj:`rpn_cls_loss`: RPN classification loss(물체의 존재 유무).
+    * :obj:`roi_loc_loss`: head module(ROI)의 classification loss(class probability).
+    * :obj:`roi_cls_loss`: head module(ROI)의 regression loss.
     * :obj:`total_loss`: The sum of 4 loss above.
 
     Args:
@@ -41,25 +41,25 @@ class FasterRCNNTrainer(nn.Module):
 
     def __init__(self, faster_rcnn):
         super(FasterRCNNTrainer, self).__init__()
-        # pytorch의 nn.Module에서 상속받아 생성함수 생성.
+        # pytorch의 nn.Module에서 상속받아 생성자 호출.
         self.faster_rcnn = faster_rcnn
         self.rpn_sigma = opt.rpn_sigma
         self.roi_sigma = opt.roi_sigma
         # L1 smooth loss를 위해서 hyperparameter로 sigma 설정.
         # target creator create gt_bbox gt_label etc as training targets. 
-        self.anchor_target_creator = AnchorTargetCreator()
-        self.proposal_target_creator = ProposalTargetCreator()
+        self.anchor_target_creator = AnchorTargetCreator() #Anchor box를  ground truth 박스 주변에 생성한다.
+        self.proposal_target_creator = ProposalTargetCreator() #ground truth box를 roi에 할당한다.
 
         self.loc_normalize_mean = faster_rcnn.loc_normalize_mean
         self.loc_normalize_std = faster_rcnn.loc_normalize_std
-
-        self.optimizer = self.faster_rcnn.get_optimizer()
+        #faster_rcnn 객체의 get_optimizer 메쏘드를 호출,learning rate, weight_decay 설정, optimizer 설정
+        self.optimizer = self.faster_rcnn.get_optimizer() 
         # visdom wrapper
         self.vis = Visualizer(env=opt.env)
 
         # indicators for training status
-        self.rpn_cm = ConfusionMeter(2)
-        self.roi_cm = ConfusionMeter(21)
+        self.rpn_cm = ConfusionMeter(2) #물체의 존재 유무 2개에관한 confusion matrix 객체 생성
+        self.roi_cm = ConfusionMeter(21)#물체의 종류 20개+1(background)에관한 confusion matrix 객체 생성
         self.meters = {k: AverageValueMeter() for k in LossTuple._fields}
         # visdom을 위해 confusionmeter를 사용해 confusion matrix를 만든다.
         # AverageValueMeter를 이용해 loss에 대한 평균을 구한다. (내부적으로 분산도 구함.)
@@ -68,22 +68,15 @@ class FasterRCNNTrainer(nn.Module):
 
         Here are notations used.
 
-        * :math:`N` is the batch size.
-        * :math:`R` is the number of bounding boxes per image.
+        * :math:`N` 배치사이즈 크기를 의미
+        * :math:`R` 이미지 당 bounding box의 갯수
 
-        Currently, only :math:`N=1` is supported.
 
         Args:
-            imgs (~torch.autograd.Variable): A variable with a batch of images.
-            bboxes (~torch.autograd.Variable): A batch of bounding boxes.
-                Its shape is :math:`(N, R, 4)`.
-            labels (~torch.autograd..Variable): A batch of labels.
-                Its shape is :math:`(N, R)`. The background is excluded from
-                the definition, which means that the range of the value
-                is :math:`[0, L - 1]`. :math:`L` is the number of foreground
-                classes.
-            scale (float): Amount of scaling applied to
-                the raw image during preprocessing.
+            imgs (~torch.autograd.Variable): 배치사이즈 만큼의 이미지
+            bboxes (~torch.autograd.Variable): 배치의 bounding , shape :math:`(N, R, 4)`.
+            labels (~torch.autograd..Variable): 배치의 라벨, shape=(N,R), background는 제외한다
+            scale (float): 전처리 동안 raw image에 적용되는 scale
 
         Returns:
             namedtuple of 5 losses
@@ -91,26 +84,26 @@ class FasterRCNNTrainer(nn.Module):
         n = bboxes.shape[0] # bounding box의 batch size를 가져온다.
         if n != 1:
             raise ValueError('Currently only batch size 1 is supported.')
-        # batch size가 1이라면 에러가 발생되도록 한다.
+        # batch size가 1이 아니면 에러가 발생되도록 한다.
         
         _, _, H, W = imgs.shape
         img_size = (H, W)
         
-        features = self.faster_rcnn.extractor(imgs)
-
+        features = self.faster_rcnn.extractor(imgs) #imgs의 특징을 추출한 특징맵을 생성한다
+        
+        #rpn에 특징맵, image 크기, scale 값을 집어넣어서 loss값, score 값 등을 얻는다.
         rpn_locs, rpn_scores, rois, roi_indices, anchor = \
             self.faster_rcnn.rpn(features, img_size, scale)
 
-        # Since batch size is one, convert variables to singular form
+        # 배치사이즈가 1이기 때문에 0으로 
         bbox = bboxes[0]
         label = labels[0]
         rpn_score = rpn_scores[0]
         rpn_loc = rpn_locs[0]
         roi = rois
 
-        # Sample RoIs and forward
-        # it's fine to break the computation graph of rois, 
-        # consider them as constant input
+        
+        #roi 내부에서 forward propagation을 진행
         sample_roi, gt_roi_loc, gt_roi_label = self.proposal_target_creator(
             roi,
             at.tonumpy(bbox),
@@ -124,7 +117,7 @@ class FasterRCNNTrainer(nn.Module):
             sample_roi,
             sample_roi_index)
 
-        # ------------------ RPN losses -------------------#
+        # ------------------ RPN losses 계산 -------------------#
         gt_rpn_loc, gt_rpn_label = self.anchor_target_creator(
             at.tonumpy(bbox),
             anchor,
@@ -135,10 +128,10 @@ class FasterRCNNTrainer(nn.Module):
             rpn_loc,
             gt_rpn_loc,
             gt_rpn_label.data,
-            self.rpn_sigma)
+            self.rpn_sigma) #rpn classification loss 계산
 
         # NOTE: default value of ignore_index is -100 ...
-        rpn_cls_loss = F.cross_entropy(rpn_score, gt_rpn_label.cuda(), ignore_index=-1)
+        rpn_cls_loss = F.cross_entropy(rpn_score, gt_rpn_label.cuda(), ignore_index=-1) #cross entropy방식 사용
         _gt_rpn_label = gt_rpn_label[gt_rpn_label > -1]
         _rpn_score = at.tonumpy(rpn_score)[at.tonumpy(gt_rpn_label) > -1]
         self.rpn_cm.add(at.totensor(_rpn_score, False), _gt_rpn_label.data.long())
